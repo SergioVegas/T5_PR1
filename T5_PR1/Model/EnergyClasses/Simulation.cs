89bfd2476ac3c5a42eb9527b08667b8e3e349c14
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-03-24 19:52:27 +0100

    model empezado

--- T5_PR1/Model/EnergyClasses/Simulation.cs
+++ T5_PR1/Model/EnergyClasses/Simulation.cs
@@ -1,10 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using T5._PR1._Practica_1.EnegyClass;
 
 namespace T5_PR1.Model.EnergyClasses
 {
     public class Simulation
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
         public DateTime DateSimulation { get; set; }
         public TypeEnergy EnergyType { get; set; }
         public double EnergyNeeded { get; set; }
