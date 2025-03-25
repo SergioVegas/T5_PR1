9fff3c26f351c1babd0ef1ad5fe18de2f48491da
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-03-25 16:46:52 +0100

    Models hechos

--- T5_PR1/Model/EnergyClasses/Simulation.cs
+++ T5_PR1/Model/EnergyClasses/Simulation.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CsvHelper.Configuration.Attributes;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using T5._PR1._Practica_1.EnegyClass;
 
@@ -9,14 +10,23 @@ namespace T5_PR1.Model.EnergyClasses
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
+        [Name("DataSimulacio")]
         public DateTime DateSimulation { get; set; }
+        [Name("TipusEnergia")]
         public TypeEnergy EnergyType { get; set; }
+        [Name("EnergiaNecessaria")]
         public double EnergyNeeded { get; set; }
+        [Name("CostEnergetic")]
         public double CostEnergy { get; set; }
+        [Name("PreuEnergia")]
         public double PriceEnergy { get; set; }
+        [Name("Rati")]
         public double Rati { get; set; }
+        [Name("EnergiaGenerada")]
         public double GeneratedEnergy { get; set; }
+        [Name("PreuTotal")]
         public double TotalPrice { set; get; }
+        [Name("CostTotal")]
         public double TotalCost { set; get; }
     }
 }
