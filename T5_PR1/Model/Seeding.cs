6886d1aa6ac0a939d9be3a3cbce6cdfb59f371d1
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-03-26 21:21:29 +0100

    Nueva tabla consumo de agua hecha. #13

--- T5_PR1/Model/Seeding.cs
+++ T5_PR1/Model/Seeding.cs
@@ -29,5 +29,26 @@ namespace T5_PR1.Model
                 context.SaveChanges();
             }
         } 
+        public static void SeedWaterConsumptions(ApplicationDbContext context, string csvFilePath)
+        {
+            context.Database.EnsureCreated();
+            // Llegir el csv
+            using (var reader = new StreamReader(csvFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var information = csv.GetRecords<WaterConsumptionCsv>().ToList();
+                //Afegim a la base de dades els camps que ens interesa
+                var waterConsumptions = information.Select(r => new WaterConsumption
+                {
+                    Comarca = r.Region,
+                    Municipi = r.Population,
+                    Consum = r.DomesticConsumptionCapita,
+                    Any = r.Year,
+                }).ToList();
+
+                context.WaterConsumptions.AddRange(waterConsumptions);
+                context.SaveChanges();
+            }
+        }
     }
 }
--- T5_PR1/Model/WaterConsumption.cs
+++ T5_PR1/Model/WaterConsumption.cs
@@ -3,14 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace T5_PR1.Model
 {
-    [Table("Consum d'aigua")]
+    [Table("ConsumAigua")]
     public class WaterConsumption
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? Comarca { get; set; }
-        public string? Municipi { get; set; }
+        public int? Municipi { get; set; }
         public double? Consum { get; set; }
         public int? Any { get; set; }
     }
