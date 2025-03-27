5395a3d01c01ff8de2119a028262b059f3afbd9e
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-03-27 22:22:21 +0100

    afegir IndicadorEnergia actualitzar. #12

--- T5_PR1/Pages/AddEnergyIndicator.cshtml.cs
+++ T5_PR1/Pages/AddEnergyIndicator.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using T5_PR1.Data;
 using T5_PR1.Model;
 
 namespace T5_PR1.Pages
@@ -7,15 +8,15 @@ namespace T5_PR1.Pages
     public class AddEnergyIndicatorModel : PageModel
     {
         private readonly ILogger<AddEnergyIndicatorModel> _logger;
-      
-        public AddEnergyIndicatorModel(ILogger<AddEnergyIndicatorModel> logger)
+        private readonly ApplicationDbContext _context;
+        public AddEnergyIndicatorModel(ILogger<AddEnergyIndicatorModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [BindProperty]
-        public EnergeticIndicatorCsv Indicador { get; set; }
-        public List<EnergeticIndicatorCsv> NewWEnergeticIndicator { get; set; } 
+        public EnergyIndicator energyIndicator { get; set; }
         public string Message { get; set; } //variable per donar més detalls als errors
 
 
@@ -25,48 +26,17 @@ namespace T5_PR1.Pages
             {
                 return Page();
             }
-            string filePathJson = Path.Combine("ModelData", "indicadors_energetics_cat.json"); 
+            
             try
             {
-
-                if (System.IO.File.Exists(filePathJson))
-                {
-                    string jsonFromFile = System.IO.File.ReadAllText(filePathJson);
-                    var deserializedIndicadors = System.Text.Json.JsonSerializer.Deserialize<List<EnergeticIndicatorCsv>>(jsonFromFile);
-                    NewWEnergeticIndicator = deserializedIndicadors.ToList();
-                }
-                else
-                {
-                    NewWEnergeticIndicator = new List<EnergeticIndicatorCsv>();
-                }
-              
-                //evitar inserir valors nuls al json
-                foreach (var propietat in typeof(EnergeticIndicatorCsv).GetProperties())
-                {
-                    var valorActual = propietat.GetValue(Indicador);
-                    if (valorActual == null)
-                    {
-                        if (propietat.PropertyType == typeof(string))
-                            propietat.SetValue(Indicador, "0.0%");
-                        else if (propietat.PropertyType == typeof(int))
-                            propietat.SetValue(Indicador, 0);
-                        else if (propietat.PropertyType == typeof(double))
-                            propietat.SetValue(Indicador, 0.0);
-                        else if (propietat.PropertyType == typeof(double?))
-                            propietat.SetValue(Indicador, 0.0);
-                        else if (propietat.PropertyType == typeof(DateTime))
-                            propietat.SetValue(Indicador, DateTime.Now);
-                    }
-                }
-                NewWEnergeticIndicator.Add(Indicador);
-                //UsingFiles.JsonHelperTool.WriteJsonFile(filePathJson, NewWEnergeticIndicator);
-
+                _context.EnergyIndicators.Add(energyIndicator);
+                _context.SaveChanges();
                 return RedirectToPage("/EnergyIndicator");
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error al procesar l'archiu JSON.");
-                Message = "Error al guardar indicador d'energia.";
+                _logger.LogError(ex, "Error al inserir les dades.");
+                Message = "Error al inserir les dades.";
                 return Page();
             }
         }
